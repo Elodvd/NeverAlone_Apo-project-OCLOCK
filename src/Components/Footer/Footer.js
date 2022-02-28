e21ca7788b7f6559d71a8a964e2cee7ed9569c75
FabienCallot <fab.callot@gmail.com>
2022-02-28 15:23:11 +0100

    add style mobile-first for the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,28 +6,35 @@ import instagram from '../../Doc/Logos-RS/insta_logo.png';
 import contact from '../../Doc/Logos-RS/contact_logo.png';
 
 const Footer = () => (
-  
     <div className="footer">
-        <div className="contact">
-            <img src={contact} alt ="Lien adresse de contact" className="logo-item"/>
+        <div className="footer-contact">
+            <img
+                src={contact}
+                alt="Lien adresse de contact"
+                className="logo-item"
+            />
             <p>Contactez-nous</p>
         </div>
-        <div>
+
+        <div className="footer-rs">
+            <a href="https://www.facebook.com">
+                <img className="logo-item" src={fb} alt="Facebook page" />
+            </a>
+            <a href="https://www.twitter.com">
+                <img className="logo-item" src={twitter} alt="Tweeter page" />
+            </a>
+            <a href="https://www.instagram.com">
+                <img
+                    className="logo-item"
+                    src={instagram}
+                    alt="Instagram page"
+                />
+            </a>
+        </div>
+        <div className="footer-copyright">
             <p>Copyright © 2022 NeverAlone</p>
         </div>
-        <div className="rs">
-        <a href="https://www.facebook.com">
-            <img className="logo-item" src={fb} alt ="Facebook page" />
-        </a>
-        <a href="https://www.twitter.com"> 
-            <img className="logo-item" src={twitter} alt ="Tweeter page"/>
-        </a>
-        <a href="https://www.instagram.com"> 
-            <img className="logo-item" src={instagram} alt ="Instagram page"/>  
-        </a>         
-        </div>
     </div>
-  
-)
+);
 
 export default Footer;
