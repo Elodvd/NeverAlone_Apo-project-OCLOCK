42c6e46b03f19d5e12a7d279df08a575750f6025
FabienCallot <fab.callot@gmail.com>
2022-03-01 09:46:19 +0100

    com and refacto

--- src/Components/LogInForm/LogInForm.js
+++ src/Components/LogInForm/LogInForm.js
@@ -5,104 +5,100 @@ import { useNavigate } from 'react-router-dom';
 
 //formulaire de login
 
-const LogInForm = () => { 
-  //valeur de base des inputs
-  const [emailValue, SetEmailValue] = useState("");
-  const [passwordValue, SetPasswordValue] = useState("");
-  const [rememberValue, SetRememberValue] = useState(false);
+const LogInForm = () => {
+    //valeur de base des inputs
+    const [emailValue, SetEmailValue] = useState('');
+    const [passwordValue, SetPasswordValue] = useState('');
+    const [rememberValue, SetRememberValue] = useState(false);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  //gerer les onChange des inputs et les inserer dans le state
+    //gerer les onChange des inputs et les inserer dans le state
 
-  const handleEmail = (event) => {
-    SetEmailValue(event.target.value);
-  }
+    const handleEmail = (event) => {
+        SetEmailValue(event.target.value);
+    };
 
-  const handlePassword = (event) => {
-    SetPasswordValue(event.target.value);
-  }
+    const handlePassword = (event) => {
+        SetPasswordValue(event.target.value);
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    SetEmailValue("");
-    SetPasswordValue("");
-    navigate("/events");
-  }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        SetEmailValue('');
+        SetPasswordValue('');
+        navigate('/events');
+    };
 
-  const handleRemember = (event) => {
-    console.log(event);
-    SetRememberValue(!rememberValue)
-  }
+    const handleRemember = (event) => {
+        console.log(event);
+        SetRememberValue(!rememberValue);
+    };
 
-  
+    return (
+        <div className="login">
+            <h1 className="login-title">
+                {' '}
+                Se connecter sur Never <span className="green">Alone</span>{' '}
+            </h1>
 
-    return(
-      <div className="login">
-      <h1 className="login-title"> Se connecter sur Never <span className="green">Alone</span> </h1>
+            <h2 className="login-subtitle">
+                Une <span className="green">application</span> sympa pour
+                partager des bons moments !
+            </h2>
 
+            <form
+                onSubmit={handleSubmit}
+                action="/login"
+                method="POST"
+                className="login-form"
+            >
+                <div className="login-form-group">
+                    <input
+                        onChange={handleEmail}
+                        value={emailValue}
+                        type="email"
+                        className="login-input"
+                        id="email"
+                        name="email"
+                        aria-describedby="emailHelp"
+                        placeholder="Votre e-mail"
+                    />
+                </div>
 
-      <h2 className="login-subtitle">Une <span className="green">application</span> sympa pour partager des bons moments !</h2>
+                <div className="login-form-group">
+                    <input
+                        onChange={handlePassword}
+                        value={passwordValue}
+                        type="password"
+                        className="login-input"
+                        id="password"
+                        name="password"
+                        placeholder="Votre mot de passe"
+                    />
+                </div>
 
+                {/* checkbox se souvenir de moi */}
 
+                <div className="login-form-remember">
+                    <input
+                        onChange={handleRemember}
+                        checked={rememberValue}
+                        className="login-input"
+                        type="checkbox"
+                    />
 
-      <form 
-        onSubmit={handleSubmit}
-        action="/login" 
-        method="POST" 
-        className="login-form"
-      >
+                    <p className="login-alert">
+                        Se souvenir de<span className="green"> moi</span>
+                    </p>
+                </div>
 
-
-        <div className="login-form-group">
-          <input 
-          onChange={handleEmail}
-          value={emailValue}
-          type="email" 
-          className="login-input" 
-          id="email" 
-          name="email" 
-          aria-describedby="emailHelp"
-          placeholder="Votre e-mail" 
-          />
+                <div className="login-form-group">
+                    <button className="login-button">C'est parti !</button>
+                </div>
+            </form>
         </div>
-
-        <div className="login-form-group">
-          <input 
-          onChange={handlePassword}
-          value={passwordValue}
-          type="password" 
-          className="login-input" 
-          id="password" 
-          name="password" 
-          placeholder="Votre mot de passe" />
-        </div>
-
-        {/* checkbox se souvenir de moi */}
-
-        <div className="login-form-remember">
-          <input 
-          onChange={handleRemember}
-          checked={rememberValue}
-          className="login-input" 
-          type="checkbox"
-          />
-
-          <p className="login-alert">Se souvenir de<span className="green"> moi</span></p>              
-        </div>
-
-        <div className="login-form-group">
-          <button 
-          className="login-button"
-          >
-            C'est parti !
-          </button>
-        </div>
-
-      </form>
-
-    </div>
-    )
-}
+    );
+};
 
 export default LogInForm;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import './Navbar.scss';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import React from 'react';
+// package pour menu burger
 import Hamburger from 'hamburger-react';
 
 const Navbar = () => {
@@ -13,14 +14,14 @@ const Navbar = () => {
         setIsOpen(false);
     };
 
-    //TODO: Trouver une solution pour le click out pour fermer la modal 
+    //TODO: Trouver une solution pour le click out pour fermer la modal
     // tout en pouvant clicker sur les liens de la modal
     // const closeModal = document.querySelector('.sign-up-btn');
 
     // document.addEventListener('mousedown', (event) => {
 
     //    closeModal && setIsOpen(false);
-        
+
     // });
 
     return (
@@ -29,46 +30,50 @@ const Navbar = () => {
                 {' '}
                 Never <span className="logo-span">Alone</span>{' '}
             </NavLink>
-            <Hamburger duration={.6} rounded toggled={isOpen} toggle={setIsOpen} />
+            <Hamburger
+                rounded
+                duration={0.6}
+                toggled={isOpen}
+                toggle={setIsOpen}
+            />
 
             {isOpen && ( //si on clique sur la modale ca ouvre ceci
                 <div className="modal-nav">
+                    {isConnected ? ( // si l'user est connecté on affiche ces liens/boutons
+                        <div className="btn-container">
+                            <NavLink
+                                onClick={handleCloseModal}
+                                className="sign-btn sign-up-btn"
+                                to="/signin"
+                            >
+                                <span className="logo-span">M</span>on Profil
+                            </NavLink>
 
-                        {
-                            isConnected ? // si l'user est connecté on affiche ces liens/boutons
-                                <div className="btn-container">
-                                    <NavLink
-                                        onClick={handleCloseModal}
-                                        className="sign-btn sign-up-btn"
-                                        to="/signin"
-                                    >
-                                        <span className="logo-span">M</span>on Profil
-                                    </NavLink>
+                            <NavLink
+                                onClick={handleCloseModal}
+                                className="sign-btn sign-in-btn"
+                                to="/login"
+                            >
+                                <span className="logo-span">E</span>venements
+                            </NavLink>
 
-                                    <NavLink
-                                        onClick={handleCloseModal}
-                                        className="sign-btn sign-in-btn"
-                                        to="/login"
-                                    >
-                                        <span className="logo-span">E</span>venements
-                                    </NavLink>
-
-                                    <NavLink
-                                        onClick={handleCloseModal}
-                                        className="sign-btn sign-in-btn"
-                                        to="/login"
-                                    >
-                                        <span className="logo-span">S</span>e déconnecter
-                                    </NavLink>
-                                </div>
-
-                            : // sinon on affiche ces boutons/liens la
-                            <div className="btn-container">
-                                <NavLink
-                                    onClick={handleCloseModal}
-                                    className="sign-btn sign-up-btn"
-                                    to="/signin"
-                                >
+                            <NavLink
+                                onClick={handleCloseModal}
+                                className="sign-btn sign-in-btn"
+                                to="/login"
+                            >
+                                <span className="logo-span">S</span>e
+                                déconnecter
+                            </NavLink>
+                        </div>
+                    ) : (
+                        // sinon on affiche ces boutons/liens la
+                        <div className="btn-container">
+                            <NavLink
+                                onClick={handleCloseModal}
+                                className="sign-btn sign-up-btn"
+                                to="/signin"
+                            >
                                 <span className="logo-span">S'</span>inscrire
                             </NavLink>
 
@@ -79,11 +84,8 @@ const Navbar = () => {
                             >
                                 <span className="logo-span">S</span>e connecter{' '}
                             </NavLink>
-                            </div>
-                        }
-
-                        
-                    
+                        </div>
+                    )}
                 </div>
             )}
         </nav>
