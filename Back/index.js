258ebfe72484a41a8d0f8d31f06708b4828937fb
Elodvd <elo@davml.org>
2022-03-01 10:36:56 +0100

    add dotenv package

--- Back/index.js
+++ Back/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const port = process.env.PORT || 3002;
+const url = process.env.URL
 const app = express();
 
 // package pour gestion de token user
@@ -10,60 +11,60 @@ const jwt = require ('jsonwebtoken');
 
 //**************************** */
 
+console.log(process.env.ACCESS_TOKEN_SECRET);
 
 
 
+// app.use(express.json()); // config de base pour récupération de caractères
+// app.use(express.urlencoded({extended:true}));
 
-app.use(express.json()); // config de base pour récupération de caractères
-app.use(express.urlencoded({extended:true}));
 
+// const user= {
+//    // GET USER FROM BACK
+// };
 
-const user= {
-    GET USER FROM BACK
-};
+// function generateAccesToken(user){
+//     return jwt.sign(user.process.env.ACCESS_TOKEN_SECRET, {expireIn:'1800s'});
+// }
 
-function generateAccesToken(user){
-    return jwt.sign(user.process.env.ACCESS_TOKEN_SECRET, {expireIn:'1800s'});
-}
+// const accesToken = generateAccesToken(user);
 
-const accesToken = generateAccesToken(user);
+// //statut non autorisé
+// app.post('/api/login', (req, res)=> {
+// if (req.body.email !== user.email){
+//     res.status(401).send('Email invalide ou non connu'); 
+//     return
+// }
+// if(req.body.password !== user.password){
+//     res.status(401).send('Mot de passe incorect');
+//     return
+// }
 
-//statut non autorisé
-app.post('/api/login', (req, res)=> {
-if (req.body.email !== user.email){
-    res.status(401).send('Email invalide ou non connu'); 
-    return
-}
-if(req.body.password !== user.password){
-    res.status(401).send('Mot de passe incorect');
-    return
-}
+// const accesToken = generateAccesToken(user);
+// res.send({
+//     accesToken,
+// });
+// });
 
-const accesToken = generateAccesToken(user);
-res.send({
-    accesToken,
-});
-});
+// function authenticateToken(req, res, next){
+// const authHeader = req.headers['authorization'];
+// const token = authHeader && authHeader.split('')[1]; // on doit passer le jwt dans le header avec comme valeur Bearer, convention de nomage
 
-function authenticateToken(req, res, next){
-const authHeader = req.headers['authorization'];
-const token = authHeader && authHeader.split('')[1]; // on doit passer le jwt dans le header avec comme valeur Bearer, convention de nomage
+// if (!token){
+//     return res.sendStatus(401);
+// }
+// jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+//  if(err){
+//     return res.sendStatus(401);
+//  }
+//  req.user = user; //si pas d'erreur on récupère le user
+//  next(); //on appelle next pour que ca passe dans la logique d ela route API
+// });
+// }
 
-if (!token){
-    return res.sendStatus(401);
-}
-jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
- if(err){
-    return res.sendStatus(401);
- }
- req.user = user; //si pas d'erreur on récupère le user
- next(); //on appelle next pour que ca passe dans la logique d ela route API
-});
-}
-
-app.get ('/api.me', authenticateToken, (req, res)=>{
-    res.send(req.user); // on retourne le user
-})
+// app.get ('/api.me', authenticateToken, (req, res)=>{
+//     res.send(req.user); // on retourne le user
+// })
 
 //**************************** */
 
