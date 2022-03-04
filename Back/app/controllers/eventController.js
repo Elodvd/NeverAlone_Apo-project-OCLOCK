6d70daa5fc5cd3a7b26ed77445bfb80d8cfe4164
candy catherine <catherine.candy974@gmail.com>
2022-03-04 17:34:05 +0100

    create EventController, test event path and change database

---
+++ Back/app/controllers/eventController.js
@@ -0,0 +1,80 @@
+const { Event } = require('../models');
+
+
+const eventController = {
+  //Recovery any events
+  async getAll(req, res, next) {
+    try {
+      const events = await Event.findAll();
+      res.json(events);
+    } catch (err) {
+      next(err);
+    }
+  },
+  //Recovery of one event
+  async getOne(req, res, next) {
+    try {
+      const id = parseInt(req.params.id, 10);
+      if (isNaN(id) || id < 1) {
+        return next();
+      }
+      const event = await Event.findByPk(id);
+      if (!event) {
+        return res.status(404).json({ error: `L'évenement n'existe pas'` });
+      }
+      res.json(event);
+    } catch (err) {
+      next(err);
+    }
+  },
+  //Creating an event
+  async create(req, res, next) {
+    try {
+      const event = await Event.create(req.body, {
+        silent: true,
+      });
+      res.json(event);
+    } catch (err) {
+      next(err);
+    }
+  },
+  //change of one event
+  async update(req, res, next) {
+    try {
+      const id = parseInt(req.params.id, 10);
+      if (isNaN(id) || id < 1) {
+        return next();
+      }
+      const result = await Event.update(req.body, {
+        where: {
+          id,
+        },
+        returning: true,
+      });
+      const event = result[1][0];
+      res.json(event);
+    } catch (err) {
+      next(err);
+    }
+  },
+  //delete of one event
+  async delete(req, res, next) {
+    try {
+      const id = parseInt(req.params.id, 10);
+      if (isNaN(id) || id < 1) {
+        return next();
+      }
+      const result = await Event.destroy({
+        where: { id },
+      });
+      if (!result) {
+        return res.status(404).json({ error: `L'évènement n'existe pas` });
+      }
+      res.status(204).json();
+    } catch (err) {
+      next(err);
+    }
+  },
+};
+
+module.exports = eventController;
--- Back/app/routers/index.js
+++ Back/app/routers/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 //const mainController = require('../controllers/mainController');
-//const errorController = require('../controllers/errorController');
+const errorController = require('../controllers/errorController');
+const eventController = require('../controllers/eventController');
 
 const router = express.Router();
 
@@ -14,15 +15,23 @@ router.post('/login', userController.loginAction);
 //signin path
 router.post('/signin', userController.signinAction);
 
+router.route('/events')
+    .get(eventController.getAll)
+    .post(eventController.create);
+
+router.route('/events/:id')
+    .get(eventController.getOne)    
+    .patch(eventController.update)
+    .delete(eventController.delete);
 
 //route pour le profil d'un utilisateur
-//router.delete('/prof/:id', userController.deleteAction);
+//router.delete('/profils/:id', userController.deleteAction);
 
 
-//router.use(errorController.error);
+router.use(errorController.error);
 
 // Answer 404
-//router.use(errorController.notFound);
+router.use(errorController.notFound);
 
 module.exports = router;
 
