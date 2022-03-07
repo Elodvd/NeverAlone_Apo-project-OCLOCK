e21a218cb29fe7266f51680f2cd2514ae29035d7
Raphaelhngstn <raphael.honig@gmail.com>
2022-03-07 15:32:11 +0100

    add create card event and img category

--- src/Components/Event/Event.js
+++ src/Components/Event/Event.js
@@ -1,31 +1,38 @@
 import './event.scss';
 //import { useState } from 'react';
-import Sport from '../../Doc/Image-Cat/sport.svg';
+
+
 
 import React from 'react'
 
-const Event = () => {
+const Event = ({item}) => {
+
+  const image = require(`../../Doc/Image-Cat/${item.category}.svg`);
+
   return (
     <div className='cardevent-container'>
         <div className='cardevent-container-header'> 
             <div>
-            <p className='cardevent-title'>Sortie escalade</p>
+            <p className='cardevent-title'> {item.title} </p>
                 <div className='cardevent-categories'>
-                    <button className='cardevent-categories-item'>Sport</button>
-                    <button className='cardevent-categories-item'>Plein Air</button>
+                    <button className='cardevent-categories-item'>{item.category}</button>
                 </div>
             </div>
-            <img src={Sport} alt="categorie-sport" className='cardevent-img'/>
+
+            <img src={image} alt={item.category} className="cardevent-img" />
+         
+
+        
                 
             
         </div>
         
-        <p className='cardevent-description'>Je vous propose une sortie escalade en nature dans le massif de Bavella. Tous niveaux acceptés. Nous pourrons définir ensemble qui amène quel matériel afin de pouvoir équiper plusieurs voies en fonction du nombre d'inscrits. Prévoir ses chaussons et son baudrier. Et n'oubliez pas le pique-nique ! </p>
+        <p className='cardevent-description'>{item.description} </p>
         
-        <h2 className='cardevent-date'>Le samedi 06 mars à 10h</h2>
-        <p className='cardevent-adress'>Massif de Bavella - Corse</p>
-        <button className='cardevent-price'>GRATUIT</button>
-        <p className='cardevent-capacity'>Places disponibles : 12</p>
+        <h2 className='cardevent-date'>{item.date}</h2>
+        <p className='cardevent-adress'>{item.adress}</p>
+        <button className='cardevent-price'>{item.price}</button>
+        <p className='cardevent-capacity'>Places disponibles : {item.capacity}</p>
         <button className='cardevent-participate'>JE PARTICIPE</button>
    
       </div>
--- src/Components/ListEvent/ListEvent.js
+++ src/Components/ListEvent/ListEvent.js
@@ -5,18 +5,47 @@ import Event from '../Event/Event';
 
 // composant principale de la page /evenements
 
-const ListEvent = ({ userData }) => {
+const ListEvent = ({ userData, eventData }) => {
+
+    const data = eventData;
+    console.log(data);
     return (
         <div className="listevent">
             <h1 className="listevent-title">
-                {' '}
                 Bienvenue {userData.first_name}
             </h1>
 
             <Button text={'Créer un nouvel évènement'} route={'/add-event'} />
+
+
             
-            {/* Faire un .map*/}
-            <Event />
+
+            <div className="card-event">
+            {               
+
+                data && (
+            
+                    data.map((item, index) => {
+                    
+                    return <Event
+                        key={index}
+                        item={item}
+                    />
+                    })
+
+                )
+            }
+
+            </div>
+          
+            
+                
+        
+            
+            
+            
+
+            
         </div>
 
 
