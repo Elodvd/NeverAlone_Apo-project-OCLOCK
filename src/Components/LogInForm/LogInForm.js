a145b75fa84430cb35895479353b310a857c73bb
FabienCallot <fab.callot@gmail.com>
2022-03-15 13:46:22 +0100

    add loading on sign in

--- src/Components/LogInForm/LogInForm.js
+++ src/Components/LogInForm/LogInForm.js
@@ -4,14 +4,16 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { loginRequest } from '../../requests/loginRequest';
 import { setBearerToken } from '../../requests';
+import Loading from '../Loading/Loading';
 
 //formulaire de login
-const LogInForm = ({ handleSetIsConnected, handleSetUserData }) => {
+const LogInForm = ({ handleSetIsConnected, handleSetUserData, eventData }) => {
     //valeur de base des inputs
     const [emailValue, SetEmailValue] = useState('');
     const [passwordValue, SetPasswordValue] = useState('');
     const [rememberValue, SetRememberValue] = useState(false);
     const [errorMessage, SetErrorMessage] = useState('');
+    const [isLogged, setIsLogged] = useState(false);
 
     const navigate = useNavigate();
 
@@ -27,13 +29,12 @@ const LogInForm = ({ handleSetIsConnected, handleSetUserData }) => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         const response = await loginRequest(emailValue, passwordValue);
-        
+
         if (response.status !== 200) {
             console.log(response);
             SetErrorMessage(response.data.error);
             SetEmailValue('');
             SetPasswordValue('');
-            
         }
 
         if (response.status === 200) {
@@ -45,10 +46,10 @@ const LogInForm = ({ handleSetIsConnected, handleSetUserData }) => {
             );
             handleSetUserData(response.data.newUser);
             SetPasswordValue('');
+            setIsLogged(true);
             setTimeout(() => {
                 navigate('/events');
-              }, 1500);
-            
+            }, 1500);
         }
     };
 
@@ -59,47 +60,53 @@ const LogInForm = ({ handleSetIsConnected, handleSetUserData }) => {
 
     return (
         <div className="login">
-            <h1 className="login-title">
-                {' '}
-                Se connecter sur Never <span className="green">Alone</span>{' '}
-            </h1>
-
-            <h2 className="login-subtitle">
-                Une <span className="green">application</span> sympa pour
-                partager des bons moments !
-            </h2>
-
-            <form
-                onSubmit={handleSubmit}
-                action="/login"
-                method="POST"
-                className="login-form"
-            >
-                <div className="login-form-group">
-                    <input
-                        onChange={handleEmail}
-                        value={emailValue}
-                        type="email"
-                        className="login-input"
-                        id="email"
-                        name="email"
-                        aria-describedby="emailHelp"
-                        placeholder="Votre e-mail"
+            {isLogged ? (
+                    <Loading
+                        color={'#4682b4'}
+                        type={'spinningBubbles'}
                     />
-                </div>
-                <div className="login-form-group">
-                    <input
-                        onChange={handlePassword}
-                        value={passwordValue}
-                        type="password"
-                        className="login-input"
-                        id="password"
-                        name="password"
-                        placeholder="Votre mot de passe"
-                    />
-                </div>
-                {/* checkbox se souvenir de moi */}
-                {/* <div className="login-form-remember">
+            ) : (
+                <>
+                    <h1 className="login-title">
+                        Se connecter sur Never
+                        <span className="green">Alone</span>
+                    </h1>
+                    <h2 className="login-subtitle">
+                        Une <span className="green">application</span> sympa
+                        pour partager des bons moments !
+                    </h2>
+                    <form
+                        onSubmit={handleSubmit}
+                        action="/login"
+                        method="POST"
+                        className="login-form"
+                    >
+                        <div className="login-form-group">
+                            <input
+                                onChange={handleEmail}
+                                value={emailValue}
+                                type="email"
+                                className="login-input"
+                                id="email"
+                                name="email"
+                                aria-describedby="emailHelp"
+                                placeholder="Votre e-mail"
+                            />
+                        </div>
+                        <div className="login-form-group">
+                            <input
+                                onChange={handlePassword}
+                                value={passwordValue}
+                                type="password"
+                                className="login-input"
+                                id="password"
+                                name="password"
+                                placeholder="Votre mot de passe"
+                            />
+                        </div>
+                        {/* TODO: à implémenter */}
+                        {/* checkbox se souvenir de moi */}
+                        {/* <div className="login-form-remember">
                     <input
                         onChange={handleRemember}
                         checked={rememberValue}
@@ -111,17 +118,17 @@ const LogInForm = ({ handleSetIsConnected, handleSetUserData }) => {
                         Se souvenir de<span className="green"> moi</span>
                     </p>
                 </div> */}
-                <div className="login-form-group">
-
-
-                {
-                    errorMessage &&
-
-                    <p className="signin-error"> {errorMessage} </p>
-                }
-                    <button className="login-button">C'est parti !</button>
-                </div>
-            </form>
+                        <div className="login-form-group">
+                            {errorMessage && (
+                                <p className="signin-error"> {errorMessage} </p>
+                            )}
+                            <button className="login-button">
+                                C'est parti !
+                            </button>
+                        </div>
+                    </form>
+                </>
+            )}
         </div>
     );
 };
