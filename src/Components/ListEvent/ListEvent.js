fd55566e0cfe04f75e31b46240f6be5bc64ae807
FabienCallot <fab.callot@gmail.com>
2022-03-15 14:31:27 +0100

    style responsive eventList & eventdetail

--- src/Components/ListEvent/ListEvent.js
+++ src/Components/ListEvent/ListEvent.js
@@ -3,23 +3,19 @@ import React, { useEffect } from 'react';
 import Button from '../Button/Button';
 import Event from '../Event/Event';
 
-
 // Liste des évènements
-// On récupère le pseudo du user, les évènements qu'il a créé et on lui permet d'en créer un nouveau 
+// On récupère le pseudo du user, les évènements qu'il a créé et on lui permet d'en créer un nouveau
 const ListEvent = ({ userData, eventData, handleSetOneEvent, getAll }) => {
     const data = eventData;
 
     useEffect(() => {
         getAll();
-    },[]);
-
+    }, []);
 
     return (
         <div className="listevent">
-            <Button text={'Créer un nouvel évènement'} route={'/add-event'} />
-
             <h1 className="listevent-title">Bienvenue {userData.first_name}</h1>
-
+            <Button className={'listevent-button'} text={'Créer un nouvel évènement'} route={'/add-event'} />
             <div className="card-event">
                 {data &&
                     data.map((item, index) => {
